76bd33f85d76f3737b2effe4a178e231e212b7f8
despar <despar@microsoft.com>
2021-02-09 16:31:54 -0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+#%%
+print("this is a sample string")
+
+
+
+# %%
+int = 30
+print("The number is" + int)
+# %%
+fname="Derek"
+lname="Sparks"
+print("My first name is " + fname + " and my last name is " + lname)
+
+# %%
+fName = "Derek"
+lName = "Sparks"
+age = 52
+print(f"my name is {fName} {lName} and I am {age} years old.")
+# %%
